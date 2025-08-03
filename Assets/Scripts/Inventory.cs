30fb59bfdb6395cafae593309a74c00b113a33d8
themost1 <benjaminmost@gmail.com>
2025-08-03 18:01:30 -0400

    Implement basic inventory/resource art

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    InventorySlot rockSlot;
+
+    [SerializeField]
+    InventorySlot woodSlot;
+}
